339706bbe000494e4433548a99e739b66d9f985a
Adya Rahmat Perdana <adyarperdana@gmail.com>
2024-08-27 15:28:09 +0700

    17th commit

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DP_PASS } = process.env;
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS } = process.env;
 
 module.exports = {
   development: {
