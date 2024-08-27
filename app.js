21d921b62c605fef816a8fdfef70383fbcc819d1
Adya Rahmat Perdana <adyarperdana@gmail.com>
2024-08-27 15:01:12 +0700

    15th commit

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.get("/hello", (req, res) => {
+    res.send("adya");
+});
+
 app.use(express.json());
 app.use(require("./routes"));
 
